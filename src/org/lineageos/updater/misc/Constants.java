8b8818de4603ff211825699ea7e06a61a394bc79
beamguide <beamguide@gmail.com>
2019-02-06 08:04:08 +0700

    adapted to ancient

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -35,11 +35,11 @@ public final class Constants {
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "ro.modversion";
-    public static final String PROP_DEVICE = "ro.superior.device";
+    public static final String PROP_DEVICE = "ro.ancient.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.superior.releasetype";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "superior.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "superior.updater.uri";
+    public static final String PROP_RELEASE_TYPE = "ro.ancient.releasetype";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "ancient.updater.allow_downgrading";
+    public static final String PROP_UPDATER_URI = "ancient.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
